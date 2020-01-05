7f5048758adce7edf48d6f557b422a90e7e76e26
Oneeb Malik <oneeb@outlook.com>
2020-01-04 20:18:52 -0800

    Add white rectangle to black screen

--- src/display.rs
+++ src/display.rs
@@ -3,7 +3,28 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
 use std::time::Duration;
+
+const CHAR_SPRITES: [[u8; 5]; 16] = [
+    /*0*/ [0xf0, 0x90, 0x90, 0x90, 0xf0],
+    /*1*/ [0x20, 0x60, 0x20, 0x20, 0x70],
+    /*2*/ [0xf0, 0x10, 0xf0, 0x80, 0xf0],
+    /*3*/ [0xf0, 0x10, 0xf0, 0x10, 0xf0],
+    /*4*/ [0x90, 0x90, 0xf0, 0x10, 0x10],
+    /*5*/ [0xf0, 0x80, 0xf0, 0x10, 0xf0],
+    /*6*/ [0xf0, 0x80, 0xf0, 0x90, 0xf0],
+    /*7*/ [0xf0, 0x10, 0x20, 0x40, 0x40],
+    /*8*/ [0xf0, 0x90, 0xf0, 0x90, 0xf0],
+    /*9*/ [0xf0, 0x90, 0xf0, 0x10, 0xf0],
+    /*a*/ [0xf0, 0x90, 0xf0, 0x90, 0x90],
+    /*b*/ [0xe0, 0x90, 0xe0, 0x90, 0xe0],
+    /*c*/ [0xf0, 0x80, 0x80, 0x80, 0xf0],
+    /*d*/ [0xe0, 0x90, 0x90, 0x90, 0xe0],
+    /*e*/ [0xf0, 0x80, 0xf0, 0x80, 0xf0],
+    /*f*/ [0xf0, 0x80, 0xf0, 0x80, 0x80]
+];
+                      
 
 const WINDOW_WIDTH: u32 = 64;
 const WINDOW_HEIGHT: u32 = 32;
@@ -21,15 +42,17 @@ pub fn draw() {
  
     let mut canvas = window.into_canvas().build().unwrap();
  
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    let _ = canvas.fill_rect(Rect::new(32*SCALE_FACTOR as i32, 16*SCALE_FACTOR as i32, SCALE_FACTOR as u32, SCALE_FACTOR as u32));
     canvas.present();
+
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
